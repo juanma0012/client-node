68cfe74245d2b973643a71e3d38dd792abc25519
Juan Sanchez <juanma0012@gmail.com>
2019-02-19 21:55:37 -0500

    Added -y to install httpd

--- cookbooks/mirror-repo/recipes/mirror_repo_config.rb
+++ cookbooks/mirror-repo/recipes/mirror_repo_config.rb
@@ -2,5 +2,5 @@ execute 'custom_repository' do
     command 'yum repolist'
     command 'yum update'
     command "yum --disablerepo='*' --enablerepo='mirrorrepo' list available"
-    command "yum --disablerepo='*' --enablerepo='mirrorrepo' install httpd"
+    command "yum --disablerepo='*' --enablerepo='mirrorrepo' install httpd -y"
 end
